a90900b2243dcd39efda2c6dff0b9e324c0d212e
jpknoll <jpknoll@ucdavis.edu>
2012-06-13 10:05:20 -0700

    Initial Commit

---
+++ Goniometer Controller/Functions/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Goniometer_Controller
+{
+    static internal class StringExtensions
+    {
+        public static string Multiply(this string source, int multiplier)
+        {
+            StringBuilder sb = new StringBuilder(multiplier * source.Length);
+            for (int i = 0; i < multiplier; i++)
+            {
+                sb.Append(source);
+            }
+
+            return sb.ToString();
+        }
+
+
+        public static string Multiply(this char source, int multiplier)
+        {
+            StringBuilder sb = new StringBuilder(multiplier);
+            for (int i = 0; i < multiplier; i++)
+            {
+                sb.Append(source);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Goniometer Controller/MotorSocket.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net;
+
+using Com6srvr;
+
+namespace Goniometer_Controller
+{
+    class MotorSocket : IDisposable
+    {
+        INet socket;
+        
+        public MotorSocket(IPAddress ipaddress)
+        {
+            socket = new NetClass();
+            socket.Connect(ipaddress.ToString());
+            socket.FSEnabled = true;
+
+            this.Connect();
+        }
+
+        private void Connect()
+        {
+            string cmd = "";
+
+            cmd += "eres8192,8192:";
+            cmd += "lh0,0:";
+            cmd += "scla9102,100:";
+            cmd += "sclv9102,100:";
+            cmd += "scld9102.2,2000:";
+            cmd += "smper4,15:";
+            cmd += "sgp20,1:";           //Proportional [microvolts/step]
+            cmd += "sgv3,15:";           //Velocity or Derivative [microvolts/step/sec]
+            cmd += "sgi,0.1:";           //Integral [microvolts/sec*sec]
+            cmd += "sgilim,2:";          //Limit?
+
+            Write(cmd);
+        }
+
+        public void Write(string cmd)
+        {
+            socket.Write(cmd);
+            socket.Flush();
+        }
+
+        public void WriteBlocking(string cmd, short timeout)
+        {
+            socket.WriteBlocking(cmd, timeout);
+        }
+
+        public string Read()
+        {
+            return socket.Read();
+        }
+
+        public void Dispose()
+        {
+            //attempt to kill motors on termination
+            if (socket != null)
+            {
+                socket.Write("!k:");
+                socket = null;
+            }
+        }
+    }
+}
